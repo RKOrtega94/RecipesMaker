e1a0f3d6adf3338f1dabbf21fd11a6f4c6679742
Robinson K. Ortega <rkortega1994@gmail.com>
2019-04-22 00:37:23 -0500

    menu

---
+++ src/Main.kt
@@ -0,0 +1,33 @@
+fun main (arg: Array<String>){
+    println(":: Bienvenido a Recipe Maker ::")
+    //Variable para almacenar el valor obtenido
+    var option = 0
+
+    //Ciclo de entrada
+    loop@ while (option != 3) {
+        println(
+            """
+
+        Elija la opcion deseada
+        1. Hacer una receta
+        2. Ver mis recetas
+        3. Salir
+        """.trimIndent()
+        )
+        print("Opcion:")
+        val resopinse: Int = readLine()!!.toInt()
+        option = resopinse
+        //Si no esta dentro de las opciones repite el ciclo
+        if (option in 4..0) continue@loop
+        //Si esta dentro de las opciones ejecuta el ciclo que corresponde
+        when (option) {
+            1 -> addRecipe(receta = readLine()!!)
+            2 -> println("Ver recetas")
+            3 -> println("Salir")
+        }
+    }
+}
+
+fun addRecipe(receta: String){
+    println(receta)
+}
